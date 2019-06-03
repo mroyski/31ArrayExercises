26eda4fc3b025f081fd1b558b3f2cfc1ccae7675
mroyski <mroyski@gmail.com>
2019-06-02 20:06:06 -0400

    Exercise 1 complete

--- 31ArrayExercises/Program.cs
+++ 31ArrayExercises/Program.cs
@@ -10,6 +10,16 @@ namespace _31ArrayExercises
     {
         static void Main(string[] args)
         {
+            Ex1();
+        }
+        static void Ex1()
+        {
+            //Write a program in C# Sharp to store elements in an array and print it.
+            int[] arr = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
         }
     }
 }
